76c962bcc42f097b5c8658c5ee8e225c993a2508
Terror-1 <elfarhosam487@gmail.com>
2022-10-20 15:30:56 +0200

    routes setup

---
+++ Backend/App.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+//express app
+const express = require("express");
+
+//mongoose database 
+const mongoose = require("mongoose");
+
+//App variables
+const app = express();
+const Instructor = require("./models/Instructor");
+
+const courseRoutes = require("./routes/course");
+//routes
+app.get("/", (req, res) => {
+  res.status(200).send("You have everything installed!");
+});
+
+app.use('/api/course/',courseRoutes)
+
+//listen for requests
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log('Listening on port',port);
+  });
---
+++ Backend/routes/course.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+//GET ALL COURSES
+router.get('/',(req,res)=>{
+    res.json({mssg: 'Get all courses'})
+});
+module.exports = router;    
