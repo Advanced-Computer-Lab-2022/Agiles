29df3d96779725d260fb6c6b3233e7cc5f2e0424
Abdullah Ahmad Fouad <abdullahahmadfouad@gmail.com>
2022-10-21 16:42:24 +0200

    fix: router --> courserouter

--- Backend/routes/course.js
+++ Backend/routes/course.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const courserouter = express.Router();
- 
+
 //GET ALL COURSES
-router.get('/',(req,res)=>{
-    res.json({mssg: 'Get all courses'})
+courserouter.get("/", (req, res) => {
+  res.json({ mssg: "Get all courses" });
 });
-//Get single Course by id 
-router.get('/:id',(req,res)=>{
-    res.json({mssg: 'Get single course'})
+//Get single Course by id
+courserouter.get("/:id", (req, res) => {
+  res.json({ mssg: "Get single course" });
 });
-//post a course 
-router.post('/:id',(req,res)=>{
-    res.json({mssg: 'post a single course'})
+//post a course
+courserouter.post("/:id", (req, res) => {
+  res.json({ mssg: "post a single course" });
 });
-module.exports = courserouter;    
+module.exports = courserouter;
