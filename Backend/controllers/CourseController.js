87cdcd1999917202a525775fb4590ee5a6a70d9d
Hossam_Elfar <73742471+Terror-1@users.noreply.github.com>
2022-10-22 22:53:18 +0200

    Update CourseController.js

--- Backend/controllers/CourseController.js
+++ Backend/controllers/CourseController.js
@@ -17,7 +17,45 @@ const createCourse = async (req , res)=>{
     } catch (error) {
         res.status(400).json ({error : error.message})
     }
-};  
+}; 
+
+//get all the titles of the courses available including the total hours of the course and course rating
+
+const coursesDetails = async (req, res) => {
+    try {
+        const courseAttr = await Course.find({}, { title: 1, totalHourseOfCourse: 1, rating: 1, _id: 0 });
+        res.status(200).send(courseAttr);
+    } catch (err) {
+        res.status(500).json({ mssg: "can't find courses" });
+    }
+};
+
+//module.exports = coursesDetails;
 
 
-module.exports = createCourse;
+//view the price of each course
+//Should i use title also or not?
+
+
+const coursePrice = async (req, res) => {
+    try {
+        const coursePrice = await Course.find({}, { title: 1, price: 1, _id: 0 });
+        res.status(200).send(coursePrice);
+    } catch (err) {
+        res.status(500).json({ mssg: "can't find prices of courses" });
+    }
+};
+module.exports = { coursePrice, coursesDetails , createCourse};
+
+/*
+//select country
+courserouter.post("/users", async (req, res) => {
+  try {
+    const country = req.query.country;
+    res.cookie("country", country)
+    console.log(cookie["country"]);
+  } catch (err) {
+    res.status(500).json({ mssg: "can't find prices of courses" });
+  }
+});
+*/
