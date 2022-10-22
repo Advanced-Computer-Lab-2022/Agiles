f35976f1f3bd8b0b5ed4bf38ba6318fa7e507298
Yehea eldib <eldib46@gmail.com>
2022-10-22 20:43:23 +0200

    add 2 get methods to select course details

--- Backend/routes/course.js
+++ Backend/routes/course.js
@@ -1,5 +1,45 @@
 const express = require("express");
 const courserouter = express.Router();
+var cookieParser = require('cookie-parser');
+
+
+const course = require("../models/Course");
+
+//get all the titles of the courses available including the total hours of the course and course rating
+
+courserouter.get("/getCourses", async (req, res) => {
+  try {
+    const courseAttr = await course.find({}, { title: 1, totalHourseOfCourse: 1, rating: 1, _id: 0 });
+    res.status(200).send(courseAttr);
+  } catch (err) {
+    res.status(500).json({ mssg: "can't find courses" });
+  }
+});
+//view the price of each course
+//Should i use title also or not?
+courserouter.get("/getCoursePrice", async (req, res) => {
+  try {
+    const coursePrice = await course.find({}, { title: 1, price: 1, _id: 0 });
+    res.status(200).send(coursePrice);
+  } catch (err) {
+    res.status(500).json({ mssg: "can't find prices of courses" });
+  }
+});
+/*
+//select country
+courserouter.post("/users", async (req, res) => {
+  try {
+    const country = req.query.country;
+    res.cookie("country", country)
+    console.log(cookie["country"]);
+  } catch (err) {
+    res.status(500).json({ mssg: "can't find prices of courses" });
+  }
+});
+*/
+
+
+
 
 //GET ALL COURSES
 courserouter.get("/", (req, res) => {
@@ -13,4 +53,6 @@ courserouter.get("/:id", (req, res) => {
 courserouter.post("/:id", (req, res) => {
   res.json({ mssg: "post a single course" });
 });
+
+
 module.exports = courserouter;
