fd3cc0f474e73cede22d9519c6a4094dcec99b55
Terror-1 <elfarhosam487@gmail.com>
2022-10-22 18:47:37 +0200

    password encryption

--- Backend/controllers/InstructorController.js
+++ Backend/controllers/InstructorController.js
@@ -1,10 +1,17 @@
 const Instructor = require("../models/Instructor")
+const bcrypt = require("bcrypt");
 
 //create Instructor
 const createInstructor = async (req , res)=>{
     const {username , password} = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newInstructor = new Instructor({
+      username : username,
+      password : hashedPassword
+    })
     try {
-      const instructor = await  Instructor.create({username,password})
+      const instructor = await  Instructor.create(newInstructor)
       res.status(200).json(instructor )
     } catch (error) {
         res.status(400).json ({error : error.message})
