5ed6fcc28e93ddcc0fabf5dcb113bd84225a55e2
A3zooz <hmoda201@gmail.com>
2022-10-23 16:16:20 +0200

    manual merging

--- Backend/controllers/courseController.js
+++ Backend/controllers/courseController.js
@@ -1,6 +1,6 @@
-const Course = require('../models/Course')
+const Course = require("../models/Course");
 
-// filter Courses by price
+
 
 const filterCourses = async (req,res) => {
     const lowerBound = req.query['lowerBound']
@@ -66,7 +66,83 @@ const courseSearch = async (req,res) => {
     }
 
 
+const createCourse = async (req, res) => {
+  const { title, subTitles, price, description } = req.body;
+  const newCourse = new Course({
+    //exercise and promotion ? not required
+    title: title,
+    subTitles: subTitles,
+    price: price,
+    description: description,
+  });
+  try {
+    const course = await Course.create(newCourse);
+    res.status(200).json(course);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+//get all the titles of the courses available including the total hours of the course and course rating
+
+const coursesDetails = async (req, res) => {
+  try {
+    const courseAttr = await Course.find(
+      {},
+      { title: 1, totalHourseOfCourse: 1, rating: 1, _id: 1 }
+    );
+    res.status(200).send(courseAttr);
+  } catch (err) {
+    res.status(500).json({ mssg: "can't find courses" });
+  }
+};
+
+const oneCoursesDetails = async (req, res) => {
+  const cid = req.query["id"];
+  try {
+    const courseAttr = await Course.findOne(
+      { _id: cid },
+      { title: 1, totalHourseOfCourse: 1, rating: 1, _id: 1 }
+    );
+    res.status(200).send(courseAttr);
+  } catch (err) {
+    res.status(500).json({ mssg: "can't find courses" });
+  }
+};
+//module.exports = coursesDetails;
+
+//view the price of each course
+//Should i use title also or not?
+
+const coursePrice = async (req, res) => {
+  try {
+    const coursePrice = await Course.find(
+      {},
+      { title: 1, price: 1, _id: 0 }
+    ).exec();
+    res.status(200).send(coursePrice);
+  } catch (err) {
+    res.status(500).json({ mssg: "can't find prices of courses" });
+  }
+};
 module.exports = {
-    filterCourses,
+  createCourse,
+  coursePrice,
+  coursesDetails,
+  oneCoursesDetails,
+  filterCourses,
     courseSearch
-}
+};
+
+/*
+//select country
+courserouter.post("/users", async (req, res) => {
+  try {
+    const country = req.query.country;
+    res.cookie("country", country)
+    console.log(cookie["country"]);
+  } catch (err) {
+    res.status(500).json({ mssg: "can't find prices of courses" });
+  }
+});
+*/
