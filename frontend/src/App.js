dc5edf84d724032aa43c3942dca95baf095e0ba6
Terror-1 <elfarhosam487@gmail.com>
2022-10-23 12:04:50 +0200

    Navbar & home components initalization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
-
+import Navbar  from './components/Navbar';
+import Home from './pages/Home';
 function App() {
+  const title = 'page'
   return (
     <div className="App">
-     
+      <Navbar/>
+       <div className="content">
+         <Home/>
+        <h1>{title}</h1>
+       </div>
     </div>
   );
 }
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <nav className = "navbar">
+            <h1>Agiles</h1>
+            <div className="links">
+                <a href ="/">Home</a>
+                <a href = "/courses">Courses</a>
+                <a href ="/login">Log in</a>
+                <a href ="/signup">Sign up</a>
+            </div>
+
+        </nav>
+      );
+}
+ 
+export default Navbar;
