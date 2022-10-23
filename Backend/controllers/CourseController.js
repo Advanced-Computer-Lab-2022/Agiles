e0c8f8871ad40f33bd130b659bc00640d70dbea3
Abdullah Ahmad Fouad <abdullahahmadfouad@gmail.com>
2022-10-23 16:29:36 +0200

    c small

--- Backend/controllers/CourseController.js
+++ Backend/controllers/CourseController.js
@@ -1,5 +1,89 @@
 const Course = require("../models/Course");
 
+const filterCourses = async (req, res) => {
+  const lowerBound = req.query["lowerBound"];
+  const upperBound = req.query["upperBound"];
+  const subjects = req.query["subject"];
+  const ratings = req.query["rating"];
+  courses = await Course.find({
+    $or: [
+      {
+        $and: [
+          { price: { $gte: lowerBound } },
+          { price: { $lte: upperBound } },
+        ],
+      },
+      { rating: ratings },
+      { subject: subjects },
+    ],
+  })
+    .sort({ price: 1 })
+    .exec();
+
+  if (subjects && lowerBound && ratings) {
+    courses = await Course.find({
+      price: { $gte: lowerBound },
+      price: { $lte: upperBound },
+      rating: ratings,
+      subject: subjects,
+    })
+      .sort({ price: 1 })
+      .exec();
+  } else if (subjects && lowerBound) {
+    courses = await Course.find({
+      price: { $gte: lowerBound },
+      price: { $lte: upperBound },
+      subject: subjects,
+    })
+      .sort({ price: 1 })
+      .exec();
+  } else if (lowerBound && ratings) {
+    courses = await Course.find({
+      price: { $gte: lowerBound },
+      price: { $lte: upperBound },
+      rating: ratings,
+    })
+      .sort({ price: 1 })
+      .exec();
+  } else if (subjects && ratings) {
+    courses = await Course.find({ rating: ratings, subject: subjects })
+      .sort({ price: 1 })
+      .exec();
+  }
+
+  if (!courses) {
+    res.status(400).json({ error: "Empty" });
+  } else {
+    res.status(200).json(courses);
+  }
+};
+
+const courseSearch = async (req, res) => {
+  const type = req.query["type"];
+  const search = req.query["search"];
+
+  if (type == "subject") {
+    courses = await Course.find({
+      subject: { $regex: new RegExp(search, "i") },
+    });
+  } else if (type == "title") {
+    courses = await Course.find({ title: { $regex: new RegExp(search, "i") } });
+  } else if (type == "instructor") {
+    courses = await Course.find({
+      instructor: { $regex: new RegExp(search, "i") },
+    });
+  } else {
+    res.status(400).json({ error: "Wrong Type" });
+    return;
+  }
+
+  if (!courses) {
+    res.status(400).json({ error: "Empty" });
+  } else {
+    res.status(200).json(courses);
+  }
+};
+
 const createCourse = async (req, res) => {
   const { title, subTitles, price, description } = req.body;
   const newCourse = new Course({
@@ -64,6 +148,8 @@ module.exports = {
   coursePrice,
   coursesDetails,
   oneCoursesDetails,
+  filterCourses,
+  courseSearch,
 };
 
 /*
