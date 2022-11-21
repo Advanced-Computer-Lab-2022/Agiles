bba3c9e2abb4b7740a23d96dfec6eccc866e58f2
A3zooz <hmoda201@gmail.com>
2022-11-21 20:31:29 +0200

    Individual trainee exercises

--- Backend/routes/individualTraineeRouter.js
+++ Backend/routes/individualTraineeRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const IndividualTraineeRouter = express.Router();
-const {getTraineebyID,InprogressCourses} = require("../controllers/IndividualTraineeController");
+const {getTraineebyID,InprogressCourses,getExerciseGrade,compareAnswers} = require("../controllers/IndividualTraineeController");
 IndividualTraineeRouter.get("/getIndividualTraineebyId", getTraineebyID);
 IndividualTraineeRouter.get("/inprogress/:id",InprogressCourses);
 IndividualTraineeRouter.get("/getIndividualExerciseGrade",getExerciseGrade);
