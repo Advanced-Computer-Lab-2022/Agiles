e7fa20a50ac63222159e278a832cb447bab7dc75
Haggagovich <60294383+Haggag23@users.noreply.github.com>
2022-11-26 23:16:08 +0200

    pages for rating

---
+++ frontend/src/pages/Course/InstructorRating.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Modal from 'react-bootstrap/Modal'
+
+export default class InstructorRating extends Component {
+  render() {
+    return (
+      <div>InstructorRating</div>
+    )
+  }
+}
