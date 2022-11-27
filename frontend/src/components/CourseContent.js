f4ef4f1f7aa0d0ee951c27d2b8e0ec475000cd21
A3zooz <hmoda201@gmail.com>
2022-11-27 07:40:11 +0200

    added videos to subtitles

--- frontend/src/components/CourseContent.js
+++ frontend/src/components/CourseContent.js
@@ -1,6 +1,7 @@
 import style from "./CourseContent.module.css";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import Accordion from "react-bootstrap/Accordion";
+import { Link, useNavigate } from "react-router-dom";
 const CoursContent = (props) => {
   const subtitles = props.course.subtitles;
   console.log(subtitles);
@@ -14,15 +15,24 @@ const CoursContent = (props) => {
            <Accordion.Item eventKey={index}>
            <Accordion.Header>Section {index + 1}: {subtitle.subtitle}</Accordion.Header>
            <Accordion.Body>
-              <YouTubeIcon className={style["youtubeIcon"]} />
               {subtitle.link?.map((link, index) => (
-                <div className={style["link"]}>
-                  <a href={link.linkUrl} target
-                  ="_blank" rel="noreferrer">
-                    {link.linkDesc}
-                  </a>
+                <div>
+                  <div className={style['video']}>
+                    <iframe
+                      width="500"
+                      height="250"
+                      src={link.linkUrl}
+                      title="YouTube video player"
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                    ></iframe></div>
+                    <div className={style["videoDesc"]}>
+                      <h5>{link.linkDesc}</h5>
+                    </div>
                 </div>
               ))}
+              
               
              <br></br>
            </Accordion.Body>
