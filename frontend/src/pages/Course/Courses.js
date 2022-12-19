c429b437cc21880d870c7d32ffceac6ae8326e54
Abdullah Ahmad Fouad <abdullahahmadfouad@gmail.com>
2022-12-19 20:03:03 +0200

    remove filter  from admin

--- frontend/src/pages/Course/Courses.js
+++ frontend/src/pages/Course/Courses.js
@@ -54,9 +54,7 @@ const Courses = (props) => {
     setSelectAll(!selectAll);
   };
   const itemEls = useRef(new Array());
-  const handleChange = (el) => {
-    console.log(el);
-  };
+
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
@@ -65,24 +63,101 @@ const Courses = (props) => {
       if (res.ok) {
         setCourses(jsondata);
       }
+
       setIsLoading(false);
     };
+
     fetchData();
   }, [selectAll]);
   const list = useRef();
+  let promoForm = (
+    <form
+      onSubmit={handleSubmit}
+      style={{
+        // display: "grid",
+        maxWidth: "50%",
+        gap: "2rem",
+        marginTop: "20rem",
+      }}
+    >
+      <div>
+        {flag && (
+          <Alert key={alert} variant={alert}>
+            {alert == "success"
+              ? "promotion updated successfully"
+              : "error happened"}
+          </Alert>
+        )}
+        <span>Amount (%) </span>
+        <input
+          required
+          type="number"
+          value={promotion}
+          placeholder="amount.."
+          style={{
+            border: "0.5px solid black",
+            borderRadius: "0.25rem",
+            marginLeft: "0.5rem",
+            padding: "0.5rem",
+            width: "10rem",
+          }}
+          onChange={handlePromo}
+        />
+      </div>
+      <div>
+        <span>End Date </span>
+        <input
+          required
+          type="date"
+          value={enddate}
+          style={{
+            border: "0.5px solid black",
+            borderRadius: "0.25rem",
+            marginLeft: "1.7rem",
+            padding: "0.5rem",
+            width: "10rem",
+          }}
+          onChange={handleEnddate}
+        ></input>
+      </div>
+      <Button
+        variant="dark"
+        type="submit"
+        style={{
+          backgroundColor: "#a00407",
+          borderRadius: 0,
+          width: "10rem",
+          border: "none",
+        }}
+      >
+        Submit
+      </Button>
+    </form>
+  );
   return (
     <>
       {isloading ? (
         <LoadingScreen loading={true} logoSrc={spinner} />
       ) : (
         <div className={style["wrapper"]}>
-          <Filter
-            className={style["wrapper-left"]}
-            chooseMessage={chooseMessage}
-            currentMessage={message}
-            admin={props.admin}
-            selectAll={props.selectAll}
-          />
+          {!props.admin ? (
+            <Filter
+              className={style["wrapper-left"]}
+              chooseMessage={chooseMessage}
+              currentMessage={message}
+              admin={props.admin}
+              selectAll={props.selectAll}
+            />
+          ) : (
+            promoForm
+            // <Filter
+
+            //   chooseMessage={chooseMessage}
+            //   currentMessage={message}
+            //   admin={props.admin}
+            //   selectAll={props.selectAll}
+            // />
+          )}
           <section className={style["wrapper-right"]}>
             <h1>Courses</h1>
             <h2>
@@ -132,70 +207,7 @@ const Courses = (props) => {
                 );
               })}
             </div>
-            <div>
-              <form
-                onSubmit={handleSubmit}
-                style={{
-                  display: "grid",
-                  maxWidth: "50%",
-                  gap: "2rem",
-                  marginTop: "2rem",
-                }}
-              >
-                <div>
-                  {flag && (
-                    <Alert key={alert} variant={alert}>
-                      {alert == "success"
-                        ? "promotion updated successfully"
-                        : "error happened"}
-                    </Alert>
-                  )}
-                  <span>Amount (%) </span>
-                  <input
-                    required
-                    type="number"
-                    value={promotion}
-                    placeholder="amount.."
-                    style={{
-                      border: "0.5px solid black",
-                      borderRadius: "0.25rem",
-                      marginLeft: "0.5rem",
-                      padding: "0.5rem",
-                      width: "10rem",
-                    }}
-                    onChange={handlePromo}
-                  />
-                </div>
-                <div>
-                  <span>End Date </span>
-                  <input
-                    required
-                    type="date"
-                    value={enddate}
-                    style={{
-                      border: "0.5px solid black",
-                      borderRadius: "0.25rem",
-                      marginLeft: "1.7rem",
-                      padding: "0.5rem",
-                      width: "10rem",
-                    }}
-                    onChange={handleEnddate}
-                  ></input>
-                </div>
-                <Button
-                  variant="dark"
-                  type="submit"
-                  style={{
-                    backgroundColor: "#a00407",
-                    borderRadius: 0,
-                    width: "10rem",
-                    border: "none",
-                  }}
-                >
-                  Submit
-                </Button>
-              </form>
-            </div>
+            <div></div>
           </section>
         </div>
       )}
