432a61bc308a5973f2822155b9359f8925244938
Haggagovich <60294383+Haggag23@users.noreply.github.com>
2022-12-27 12:11:31 +0200

    trainee replying

--- frontend/src/pages/Course/AskInstructor.js
+++ frontend/src/pages/Course/AskInstructor.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import RegCourse from "./RegCourse";
 import axios from "axios";
+import Button from "react-bootstrap/Button";
 import regStyles from "./RegCourse.module.css";
 import style from "./AskInstructor.module.css";
 
@@ -11,11 +12,18 @@ const cookies = new Cookies();
 function AskInstructor() {
   const [questions, setQuestions] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [newReply, setNewReply] = useState(false);
   const location = useLocation();
   const index = new URLSearchParams(location.search).get("idx");
   const userId = cookies.get("currentUser");
   console.log(location.state);
 
+  const handleSubmitReply = async (e) => {
+    let res = await axios.patch("/individualtrainee/addReply", {
+      questionId: questions.id,
+      reply: newReply,
+    });
+  };
   const fetchdata = async () => {
     console.log(location.state.course_id);
     console.log(userId);
@@ -66,8 +74,53 @@ function AskInstructor() {
       </div>
       <div className={style["mainRight"]}>
         {questions.map((el) => {
-          return <div>{el.question}</div>;
+          console.log(el.replies[0].reply);
+          return (
+            <div>
+              <div>{el.question}</div>
+
+              <div>
+                {el.replies.map((replies) => {
+                  if (replies.isInstructor == false)
+                    return (
+                      <div>
+                        <div>{replies.reply}</div>
+                      </div>
+                    );
+                })}
+              </div>
+              <div>
+                {el.replies.map((replies) => {
+                  if (replies.isInstructor == true)
+                    return (
+                      <div>
+                        <div>{replies.reply}</div>
+                      </div>
+                    );
+                })}
+              </div>
+            </div>
+          );
         })}
+      </div>
+      <div>
+        <form on onSubmit={handleSubmitReply}>
+          <textarea
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+            type="txt"
+            placeholder="Reply"
+            //style={styles.input}
+            onChange={(e) => setNewReply(e.target.value)}
+            required
+          ></textarea>
+          <div>
+            <Button className={style["button"]} type="submit">
+              Send
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   );
